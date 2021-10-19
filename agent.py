ff8a4079b46ba56004a1d8805f8e4c8030aad9ed
Arun Subbaiah <41087857+arun-sp@users.noreply.github.com>
2021-10-19 12:28:36 -0400

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,12 +1,19 @@
 class Agent:
 
   def __init__(self, gridworld, type):
-    self.gridworld = gridworld
-    self.type = type
+
+    # Gridworld that the agent is linked to. Initially it will be an empty grid
+    self.gridworld = gridworld 
+    # Type 1 - Blindfolded, Type 2 - Can see on 4 directions, Type 3 - Example Inference agent
+    self.type = type # Type 1, 2, 3
+
+    # Start and goal of the agent 
     self.goal = self.gridworld.goal
     self.position = self.gridworld.start
-    self.path = []
-    self.gridworld.agent = self
+  
+    self.path = [] # For storing path, once the agent starts moving
+    self.gridworld.agent = self # Doubly linking this agent to its griworld
+
+    # Set up sensing variables - nx, cx, bx, ex, hx for agent type 3 
     if self.type == 3:
       self.gridworld.setSensing()
-    print ('New agent created...')
